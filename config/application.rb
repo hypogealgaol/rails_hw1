c181e4da1ffe04bb8baf97f72940bc373c5ad7e4
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-09-21 20:25:49 -0400

    bundled

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module Sss2208App1
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+        g.orm :mongoid
+    end
   end
 end
