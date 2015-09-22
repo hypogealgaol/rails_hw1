5eee2922ba52937deef1284d1876fcfd4161974d
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-09-22 12:33:26 -0400

    added files from emily repo/edits

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+
+  #spring
+  gem 'spring'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+
+  get "signup" => "users#new", :as => "signup"
+  get 'login' => 'session#new'
+  post "login" => "session#create"
+  get "logout" => "session#destroy", :as => "logout"
+
+  #root of the site
+  root 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
