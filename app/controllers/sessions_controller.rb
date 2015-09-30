d809e817618a742aa08df7a51979204184e92fdf
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-09-29 22:37:24 -0400

    fixing authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,20 @@ class SessionsController < ApplicationController
   skip_before_filter :require_user, :only => [:new, :create, :login]
 
   def new
-    @user = User.new
+    #@user = User.new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email_address])
+    user = User.find_by(email: params[:session][:email])
+    puts "tried to find user"
     if user
-      puts "successful login"
+      puts "successful login" #testing
       session[:user_id] = user.id
       redirect_to users_path
     else
-      flash[:notice] = "Unsuccessful Login"
-      puts "unsuccessful login"
-      render "new"
+      flash[:alert] = "Unsuccessful login"
+      puts "unsuccessful login" #testing
+      render "new" #renders a different template within the same controller
     end  
   end
   
